70edb81d94d0ae879d2d8957792994d49f3922ad
Utawachan <128827489+Utawachan@users.noreply.github.com>
2023-05-23 06:50:47 +0700

    Update rinne.sh

--- rinne.sh
+++ rinne.sh
@@ -13,6 +13,8 @@ echo "Liên hệ tạo images: https://www.facebook.com/rinne.09"
 
 echo "===================================="
 sleep 4
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update &>/dev/null && sudo apt install ngrok -y &>/dev/null
+
 clear
 echo "===================================="
 echo "===================================="
